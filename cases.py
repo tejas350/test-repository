01c23a2668d5e6113c4ac886ac2637f280de6a4d
Tejas Suryawanshi <tejassuryawanshi267@gmail.com>
2025-05-03 12:24:05 +0530

    third commit

--- cases.py
+++ cases.py
@@ -1,27 +1,27 @@
-print("Hello, World!")
+# print("Hello, World!")
 
-#checking even odd numbers
-num = 7
-if num % 2 == 0:
-    print("Even")
-else:
-    print("Odd")
+# #checking even odd numbers
+# num = 7
+# if num % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
 
-#swapping
-a, b = 5, 10
-a, b = b, a
-print(a, b)
+# #swapping
+# a, b = 5, 10
+# a, b = b, a
+# print(a, b)
 
 
-#  def factorial(n):
-#      return 1 if n == 0 else n * factorial(n-1)
+# #  def factorial(n):
+# #      return 1 if n == 0 else n * factorial(n-1)
 
-#  print(factorial(5))
+# #  print(factorial(5))
 
-nums = [4, 1, 9, 2]
-print(max(nums))
+# nums = [4, 1, 9, 2]
+# print(max(nums))
 
-# a, b = 0, 1
-# for _ in range(10):
-#     print(a, end=' ')
-#     a, b = b, a + b
+a, b = 0, 1
+for _ in range(10):
+    print(a, end=' ')
+    a, b = b, a + b
