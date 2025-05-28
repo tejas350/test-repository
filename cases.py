307b59ffb3afd8eaf4ea214643ee97f64bc1ee78
Tejas Suryawanshi <137555154+tejas350@users.noreply.github.com>
2025-05-28 11:23:33 +0530

    Update cases.py

--- cases.py
+++ cases.py
@@ -13,10 +13,10 @@ a, b = b, a
 print(a, b)
 
 
-#  def factorial(n):
-#      return 1 if n == 0 else n * factorial(n-1)
+  def factorial(n):
+      return 1 if n == 0 else n * factorial(n-1)
 
-#  print(factorial(5))
+  print(factorial(5))
 
 nums = [4, 1, 9, 2]
 print(max(nums))
